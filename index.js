997e88f2f2915ef01e90bebdb2f5193d3059d74d
Digital AGA <89376199+digitalaga@users.noreply.github.com>
2023-10-17 22:30:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = '6500047335:AAH5TKATnx0wXOzh4F2lYJqNs3Okj-4IL00'
+const token = '6694098055:AAEZs9mF8C3U438TZO4NTUXMHr7JCKJeNgs'
 const id = '1098083004'
 const address = 'https://www.google.com'
 
